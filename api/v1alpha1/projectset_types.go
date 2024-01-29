2393fc2d566f3dd2a245f3f26a6ef89b72d503c1
Anatolii Makarov <anatolii.makaroff@gmail.com>
2024-01-29 00:42:59 -0500

    init

---
+++ api/v1alpha1/projectset_types.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2024 Anatolii Makarov.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ProjectSetSpec defines the desired state of ProjectSet
+type ProjectSetSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// ProjectSet name
+	Name string `json:"name"`
+
+	// ProjectSet labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// ProjectSet annotations
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+// ProjectSetStatus defines the observed state of ProjectSet
+type ProjectSetStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Conditions store the status conditions of the instances
+	// +operator-sdk:csv:customresourcedefinitions:type=status
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
+
+// ProjectSet is the Schema for the projectsets API
+type ProjectSet struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ProjectSetSpec   `json:"spec,omitempty"`
+	Status ProjectSetStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ProjectSetList contains a list of ProjectSet
+type ProjectSetList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ProjectSet `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ProjectSet{}, &ProjectSetList{})
+}
---
+++ internal/controller/projectset_controller.go
@@ -0,0 +1,233 @@
+/*
+Copyright 2024 Anatolii Makarov.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	projectsetv1alpha1 "github.com/migrx-io/projectset-operator/api/v1alpha1"
+)
+
+// Finilizer name
+const projectSetFinalizer = "projectset.migrx.io/finalizer"
+
+// Definitions to manage status conditions
+const (
+	// Resource is Available
+	typeAvailableStatus = "Available"
+	typeDegradedStatus  = "Degraded"
+)
+
+// ProjectSetReconciler reconciles a ProjectSet object
+type ProjectSetReconciler struct {
+	client.Client
+	Scheme   *runtime.Scheme
+	Recorder record.EventRecorder
+}
+
+//+kubebuilder:rbac:groups=projectset.migrx.io,resources=projectsets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=projectset.migrx.io,resources=projectsets/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=projectset.migrx.io,resources=projectsets/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+//+kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the ProjectSet object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
+func (r *ProjectSetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+
+	log := r.Log.WithValues("projectset", req.NamespacedName)
+
+	//
+	// Fetch the instance
+	//
+	instance := &projectsetv1alpha1.ProjectSet{}
+	err := r.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// If the custom resource is not found then, it usually means that it was deleted or not created
+			// In this way, we will stop the reconciliation
+			log.Info("ProjectSet resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get ProjectSet")
+		return ctrl.Result{}, err
+	}
+
+	//
+	// Set the status as Unknown when no status are available
+	//
+	if instance.Status.Conditions == nil || len(instance.Status.Conditions) == 0 {
+
+		if err := r.setStatus(ctx, req, instance,
+			typeAvailableStatus,
+			metav1.ConditionUnknown,
+			"Reconciling",
+			"Starting reconciliation"); err != nil {
+
+			return ctrl.Result{}, err
+
+		}
+	}
+
+	//
+	// Add a finalizer. Then, we can define some operations which should
+	// occurs before the custom resource to be deleted.
+	//
+	if !controllerutil.ContainsFinalizer(instance, projectSetFinalizer) {
+
+		log.Info("Adding Finalizer")
+		if ok := controllerutil.AddFinalizer(instance, projectSetFinalizer); !ok {
+		}
+
+		if err = r.Update(ctx, instance); err != nil {
+			log.Error(err, "Failed to update custom resource to add finalizer")
+			return ctrl.Result{}, err
+		}
+	}
+
+	//
+	// Check if the instance is marked to be deleted, which is
+	// indicated by the deletion timestamp being set.
+	//
+	isMarkedToBeDeleted := instance.GetDeletionTimestamp() != nil
+
+	if isMarkedToBeDeleted {
+		if controllerutil.ContainsFinalizer(instance, projectSetFinalizer) {
+
+			log.Info("Performing Finalizer Operations before delete CR")
+
+			// Update status
+			if err := r.setStatus(ctx, req, instance,
+				typeDegradedStatus,
+				metav1.ConditionUnknown,
+				"Finalizing",
+				"Performing finilizer operations"); err != nil {
+
+				return ctrl.Result{}, err
+
+			}
+
+			// Perform all operations required before remove the finalizer and allow
+			// the Kubernetes API to remove the custom resource.
+			r.doFinalizerOperations(instance)
+
+			// Update status
+			if err := r.setStatus(ctx, req, instance,
+				typeDegradedStatus,
+				metav1.ConditionTrue,
+				"Finalizing",
+				"Finilizer operations are done"); err != nil {
+
+				return ctrl.Result{}, err
+
+			}
+
+			// Remove finilizers
+			log.Info("Removing Finalizer after successfully perform the operations")
+			if ok := controllerutil.RemoveFinalizer(instance, projectSetFinalizer); !ok {
+				log.Error(err, "Failed to remove finalizer")
+				return ctrl.Result{Requeue: true}, nil
+			}
+
+			if err := r.Update(ctx, instance); err != nil {
+				log.Error(err, "Failed to remove finalizer")
+				return ctrl.Result{}, err
+			}
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+//
+//
+// Reconcile helper functions
+//
+//
+
+func (r *ProjectSetReconciler) setStatus(ctx context.Context,
+	req ctrl.Request,
+	instance *projectsetv1alpha1.ProjectSet,
+	statusType, status, reason, message string) error {
+
+	// Set condition
+	meta.SetStatusCondition(&instance.Status.Conditions,
+		metav1.Condition{Type: statusType,
+			Status:  status,
+			Reason:  reason,
+			Message: message})
+
+	// Update state
+	if err = r.Status().Update(ctx, instance); err != nil {
+		log.Error(err, "Failed to update instance status")
+		return err
+	}
+
+	// Refetch last state
+	if err := r.Get(ctx, req.NamespacedName, instance); err != nil {
+		log.Error(err, "Failed to re-fetch instance")
+		return err
+	}
+
+	return nil
+}
+
+// Finalizers will perform the required operations before delete the CR.
+func (r *ProjectSetReconciler) doFinalizerOperations(cr *projectsetv1alpha1.ProjectSet) {
+	// TODO(user): Add the cleanup steps that the operator
+	// needs to do before the CR can be deleted. Examples
+	// of finalizers include performing backups and deleting
+	// resources that are not owned by this CR, like a PVC.
+
+	// Note: It is not recommended to use finalizers with the purpose of delete resources which are
+	// created and managed in the reconciliation. These ones, such as the Deployment created on this reconcile,
+	// are defined as depended of the custom resource. See that we use the method ctrl.SetControllerReference.
+	// to set the ownerRef which means that the Deployment will be deleted by the Kubernetes API.
+	// More info: https://kubernetes.io/docs/tasks/administer-cluster/use-cascading-deletion/
+
+	//eventtype is the type of this event, and is either Normal or Warning.
+	// The following implementation will raise an event
+	r.Recorder.Event(cr,
+		"Warning",
+		"Deleting",
+		fmt.Sprintf("Custom Resource %s is being deleted", cr.Name))
+
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ProjectSetReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&projectsetv1alpha1.ProjectSet{}).
+		WithOptions(controller.Options{MaxConcurrentReconciles: 2}).
+		Complete(r)
+}
