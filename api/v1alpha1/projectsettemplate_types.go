71524d091b87936f4a23cd2aa1314228d6fc673a
Anatolii Makarov <anatolii.makaroff@gmail.com>
2024-02-11 01:29:29 -0500

    add template

---
+++ api/v1alpha1/projectsettemplate_types.go
@@ -0,0 +1,97 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ProjectSetTemplateSpec defines the desired state of ProjectSetTemplate
+type ProjectSetTemplateSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Namespace name
+	NamespaceSuffix string `json:"namespaceSuffix"`
+
+	// Custom namespace labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// Custom namespace annotations
+	Annotations map[string]string `json:"annotations,omitempty"`
+
+	// ResourceQuota specification
+	ResourceQuota corev1.ResourceQuotaSpec `json:"resourceQuota,omitempty"`
+
+	// LimitRange specification
+	LimitRange v1.LimitRangeSpec `json:"limitRange,omitempty"`
+
+	// RBAC Role Rules
+	RoleRules map[string][]rbacv1.PolicyRule `json:"roleRules,omitempty"`
+
+	// User permissions
+	GroupPermissions map[string][]rbacv1.Subject `json:"groupPermissions,omitempty"`
+
+	// Network Policy specitifation
+	PolicySpec map[string]networkingv1.NetworkPolicySpec `json:"policySpec,omitempty"`
+}
+
+// ProjectSetTemplateStatus defines the observed state of ProjectSetTemplate
+type ProjectSetTemplateStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Conditions store the status conditions of the instances
+	// +operator-sdk:csv:customresourcedefinitions:type=status
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:singular=projectset
+//+kubebuilder:resource:scope=Cluster,shortName=pst;prjst
+//+kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+
+// ProjectSetTemplate is the Schema for the projectsettemplates API
+type ProjectSetTemplate struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ProjectSetTemplateSpec   `json:"spec,omitempty"`
+	Status ProjectSetTemplateStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ProjectSetTemplateList contains a list of ProjectSetTemplate
+type ProjectSetTemplateList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ProjectSetTemplate `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ProjectSetTemplate{}, &ProjectSetTemplateList{})
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-logr/logr v1.2.4
 	github.com/onsi/ginkgo/v2 v2.11.0
 	github.com/onsi/gomega v1.27.10
+	gopkg.in/yaml.v2 v2.4.0
 	k8s.io/api v0.27.2
 	k8s.io/apimachinery v0.27.2
 	k8s.io/client-go v0.27.2
@@ -78,7 +79,6 @@ require (
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.27.2 // indirect
 	k8s.io/component-base v0.27.2 // indirect
